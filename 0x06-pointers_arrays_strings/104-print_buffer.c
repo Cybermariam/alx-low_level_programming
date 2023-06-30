1abab04842c65cb77b4d03af7dca05d97064f5d4
Cybermariam <ademolamariam95@gmail.com>
2023-06-30 14:25:15 -0700

    function that adds two numbers

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
- * print_buffer -  prints a buffer.
+ * print_buffer - prints a buffer.
  * @b: buffer
  * @size: size
  * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
 	int o, j, i;
@@ -27,7 +28,7 @@ void print_buffer(char *b, int size)
 			if (i < j)
 				printf("%02x", *(b + o + i));
 			else
-				printf(" ");
+				printf("  ");
 			if (i % 2)
 			{
 				printf(" ");
