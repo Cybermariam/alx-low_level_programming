e6cd405a6ae08e93d2362a2fe4cb1dd066b927a6
cybermariam <ademolamariam95@gmail.com>
2023-09-01 18:42:28 -0700

    task

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,20 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
 
 /**
- * main - program that prints all arguments it receives.
- * @argc: argument count
- * @argv: argument vector
- * Return: Always 0 Success
+ * main - prints all arguments that the program receives
+ * @argc: arguments count
+ * @argv: array of arguments
+ *
+ * Return: Always 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
+	{
 		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check - string that are digit
+ * @str: array str
+ *
+ * Return: Always 0
+ */
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
 
 /**
- * main - program that adds positive numbers.
- * @argc: argument count
- * @argv: argument vector
+ * main - Print the program name
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
  * Return: Always 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
+
 {
-	int i, add = 0;
 
-	if (NULL)
-		printf("%d\n", 0);
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-	if (argc >= 'a' && argc <= 'z') || (argc >= 'A' && argc <= 'Z')
+	count = 1;
+	while (count < argc) /*goes through the whole array*/
 	{
-		printf("Error\n");
+		if (check_num(argv[count]))
+
+		{
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
+		}
+
+		/*Condition if one of the number contains symbols which are not digits*/
+		else
+		{
+			printf("Error\n");
 			return (1);
+		}
+
+		count++;
 	}
 
-	for (i = 1; i < argc; i++)
-		add += argv[i];
+	printf("%d\n", sum); /*print the sum*/
+
 	return (0);
 }
