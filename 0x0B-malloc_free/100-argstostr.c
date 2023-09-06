6c95a8ec6534265c10979dbbfe079be693f35a69
cybermariam <ademolamariam95@gmail.com>
2023-09-06 02:39:58 -0700

    updated

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -25,14 +25,16 @@ char *argstostr(int ac, char **av)
 	concatArg = (char *)malloc(sizeof(char) * lenTotal);
 	if (concatArg == NULL)
 		return (NULL);
-	concatArg[0] = '\0';
-	for (y = 0; y < ac; y++)
+	y = 0;
+	for (x = 0; x < ac; x++)
 	{
-		strcat(concatArg, av[y]);
-		if (y != ac - 1)
-		{
-			strcat(concatArg, "\n");
-		}
+		strcpy(concatArg + y, av[x]);
+		y += strlen(av[x]);
+		concatArg[y] = '\n';
+		y++;
 	}
+
+	concatArg[y - 1] = '\0';
+
 	return (concatArg);
 }
