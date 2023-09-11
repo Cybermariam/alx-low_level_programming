84de9dd4a4e274def60690acfa77b04c9c7abc02
cybermariam <ademolamariam95@gmail.com>
2023-09-11 15:54:58 -0700

    struct task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new_dog;
+
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 		return (NULL);
@@ -23,7 +24,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	strcpy(new_dog->name, name);
-	
 	new_dog->owner = malloc(strlen(owner) + 1);
 	if (new_dog->owner == NULL)
 	{
@@ -31,7 +31,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	strcpy(new_dog->owner, owner);
-	
 	new_dog->age = age;
-	return new_dog;	
+	return (new_dog);
 }
