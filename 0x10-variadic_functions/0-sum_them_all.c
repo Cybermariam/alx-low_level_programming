42f0bcd15dbac6acaad633e1ae03c5f1b186f1e8
cybermariam <ademolamariam95@gmail.com>
2023-09-14 15:29:01 -0700

    variadic func task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
 #include "variadic_functions.h"
 #include <stdarg.h>
 
 /**
- * sum_them_all - function that returns the sum of all its parameters
- * @n: argument count
- * Return: the sum
+ * sum_them_all - return the sum of all parameters
+ * @n: is theargument count
+ * Return: sum of all parameters
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int i, sum = 0;
+	va_list sum;
+	unsigned int i, result = 0;
 
 	if (n == 0)
 		return (0);
-	va_start(ap, n);
-	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-	va_end(ap);
 
-	return (sum);
+	va_start(sum, n);
+	for (i = 0; i < n; i++)
+		result += va_arg(sum, int);
+
+	va_end(sum);
+	return (result);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,30 +1,26 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_numbers - function that prints numbers
- * @separator: is the string to be printed
- * @n: number of integers
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: is the string to be printed between numbers
+ * @n: number of integers passed to the function
+ * Return: 0
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list printNum;
+	va_list print;
 	unsigned int i;
-	int num;
 
-	if (separator == NULL)
-		return;
-	va_start(printNum, n);
+	va_start(print, n);
 	for (i = 0; i < n; i++)
 	{
-		num = va_arg(printNum, int);
-		printf("%d", num);
-		if (i < n - 1)
+		printf("%d", va_arg(print, int));
+
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
-
-	va_end(printNum);
+	va_end(print);
 }
