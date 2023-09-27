ed9cafe6b1cf8f492b0ec0244610d92ce3112981
cybermariam <ademolamariam95@gmail.com>
2023-09-27 05:28:04 -0700

    function that frees a lis

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: Pointer to a pointer to the head of the list.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+
+	if (head == NULL || *head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		new = *head;
+		*head = (*head)->next;
+		free (new);
+	}
+	*head = NULL;
+}
